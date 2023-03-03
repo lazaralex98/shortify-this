8bc8282ceb8445e88fe155c0c3b867952f08590f
lazaralex98 <alex2012ro@gmail.com>
2023-03-03 21:09:46 +0200

    added tailwind and setup a basic layout

--- popup.tsx
+++ popup.tsx
@@ -1,28 +1,36 @@
-import { useState } from "react"
+import "./style.css"
 
 function IndexPopup() {
-  const [data, setData] = useState("")
-
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
-    </div>
+    <Container>
+      <Header />
+      <div className="mt-5">Form goes here.</div>
+    </Container>
   )
 }
 
 export default IndexPopup
+
+function Container({ children }) {
+  return (
+    <div className="bg-white shadow sm:rounded-lg w-72">
+      <div className="px-4 py-5 sm:p-6">{children}</div>
+    </div>
+  )
+}
+
+function Header() {
+  return (
+    <div className="border-b border-gray-200 pb-2">
+      <h3 className="text-base font-semibold leading-6 text-gray-900">
+        Shortify this.
+      </h3>
+      <div className="mt-2 max-w-xl text-sm text-gray-500">
+        <p>
+          Put in a long URL and get a short one. Use your custom slug if you
+          want.
+        </p>
+      </div>
+    </div>
+  )
+}
